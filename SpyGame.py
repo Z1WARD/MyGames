51dfc342e3dbd4629c0957a9e827182f092e190b
Veselov Boris <30696447+Z1WARD@users.noreply.github.com>
2019-03-17 13:11:03 +0700

    Update SpyGame.py

--- SpyGame.py
+++ SpyGame.py
@@ -13,6 +13,8 @@ stand = pygame.image.load('R1.png')
 
 clock = pygame.time.Clock()
 
+score = 0
+
 
 class Player(object):
     def __init__(self, x, y, width, height):
@@ -47,7 +49,6 @@ class Player(object):
             else:
                 window.blit(walkRight[0], (self.x, self.y))
         self.hitbox = (self.x + 17, self.y + 10, 78, 140)
-        pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
 
 
 class Enemy(object):
@@ -66,21 +67,24 @@ class Enemy(object):
         self.speed = 5
         self.path = [self.x, self.end]
         self.hitbox = (self.x + 20, self.y, 28, 60)
+        self.health = 2
+        self.visible = True
 
     def draw(self, window):
         self.move()
-        if self.walkCount + 1 >= 16:
-            self.walkCount = 0
+        if self.visible:
+            if self.walkCount + 1 >= 16:
+                self.walkCount = 0
 
-        if self.speed > 0:
-            window.blit(self.walkRight[self.walkCount // 3], (self.x, self.y))
-            self.walkCount += 1
-        else:
-            window.blit(self.walkLeft[self.walkCount // 3], (self.x, self.y))
-            self.walkCount += 1
-        self.hitbox = (self.x + 15, self.y + 12, 50, 85)
-        pygame.draw.rect(window, (255, 0, 0), self.hitbox, 2)
-
+            if self.speed > 0:
+                window.blit(self.walkRight[self.walkCount // 3], (self.x, self.y))
+                self.walkCount += 1
+            else:
+                window.blit(self.walkLeft[self.walkCount // 3], (self.x, self.y))
+                self.walkCount += 1
+            self.hitbox = (self.x + 15, self.y + 12, 50, 85)
+            pygame.draw.rect(window, (255, 0, 0), (self.hitbox[0], self.hitbox[1] - 30, 50, 10))
+            pygame.draw.rect(window, (0, 255, 0), (self.hitbox[0], self.hitbox[1] - 30, 50 - ((50/3) * (2 - self.health)), 10))
 
     def move(self):
         if self.speed > 0:
@@ -97,6 +101,11 @@ class Enemy(object):
                 self.walkCount = 0
 
     def hit(self):
+        if self.health > 0:
+            self.health -= 1
+        else:
+            self.health -= 1
+            self.visible = False
         print("HIT!")
 
 
@@ -117,6 +126,8 @@ def update_game_window():
     window.blit(bg, (0, 0))
     spy.draw(window)
     soldier.draw(window)
+    text = font.render('Score: ' + str(score), 1, (5, 5, 5))
+    window.blit(text, (0, 10))
 
     for bullet in bullets:
         bullet.draw(window)
@@ -124,6 +135,7 @@ def update_game_window():
     pygame.display.update()
 
 
+font = pygame.font.SysFont('comicsans', 40, True, True)
 spy = Player(300, 410, 64, 64)
 soldier = Enemy(100, 475, 64, 64, 1000)
 bullets = []
@@ -139,6 +151,10 @@ while run:
         if bullet.y - bullet.radius < soldier.hitbox[1] + soldier.hitbox[3] and bullet.y + bullet.radius > soldier.hitbox[1]:
             if bullet.x + bullet.radius > soldier.hitbox[0] and bullet.x - bullet.radius < soldier.hitbox[0] + soldier.hitbox[2]:
                 soldier.hit()
+                score += 1
+                if soldier.health < 0:
+                    soldier = Enemy(0, 0, 0, 0, 0)
+                    soldier.visible = False
                 bullets.pop(bullets.index(bullet))
 
         if 15 < bullet.x < 1145:
